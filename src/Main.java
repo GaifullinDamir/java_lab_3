483dd1c2ab5b8a6165d79dacab7c57bd5b76e771
Name <damiros.20002@gmail.com>
2023-03-06 15:44:16 +0300

    Работа над считыванием файлов

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
 
+public class Main {
+    private final static Processor _processor = new Processor();
+    public static void main(String[] args) {
+        int[] resultArr;
+
+        try{
+            _processor.CheckArrSize(args);
+            _processor.CheckCurrSymb(args);
+            resultArr = _processor.GetSumEvenOddNumbers(args);
+            _processor.PrintOddAndEvenNums(resultArr);
+        } catch(ExcArrElementNotEnough e) {
+            System.out.println(e);
+        } catch(ExcMissingNumZero e) {
+            System.out.println(e);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
 
     }
 }
--- src/Reader.java
+++ src/Reader.java
@@ -1,5 +1,39 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.*;
 import java.util.*;
 
 public class Reader {
-    Scanner sc = new Scanner(System.in);
+    private static Scanner _sc;
+
+    public Reader(){
+        _sc = new Scanner(System.in);
+    }
+
+    public static String[] readConsoleStringArgs(){
+        String resultStr;
+        resultStr = _sc.nextLine();
+        String[] resultStrArr = resultStr.split(" ");
+
+        return resultStrArr;
+    }
+
+    public static String[] readFileStringArgs(String filePath){
+        String[] resultStrArr;
+        try{
+            BufferedReader br = new BufferedReader (new FileReader(filePath));
+
+            String inStr;
+
+            if((inStr = br.readLine()) != null){
+                resultStrArr = inStr.split(" ");
+                return resultStrArr;
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        resultStrArr = new String[]{};
+        return resultStrArr;
+    }
 }
